a5cd986b77c7aa077bdb3d3a7c4611fc790c0197
somoye123 <somoye.ayotunde@gmail.com>
2020-11-05 22:44:50 +0100

    add validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates_uniqueness_of :username
+    validates :username, :email, presence: true
+    validates :email, format: {with: VALID_EMAIL_REGEX}, uniqueness: {:case_sensitive => false}
 end
