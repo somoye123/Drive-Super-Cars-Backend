c0455fa7f734fb7e6432f9345e2dd7812631dd10
somoye123 <somoye.ayotunde@gmail.com>
2020-11-05 23:02:24 +0100

    refactor users controller to allow create action only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
-class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
-    render json: @user
-  end
+class UsersController < SecuredController
 
   def create
+    @user = User.find_by(email: params[:email])
+    return render json: @user, status: :ok if @user
+    
     @user = User.new(user_params)
 
     if @user.save
