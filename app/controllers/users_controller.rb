92a30fdb9dac75697adce2087e00165f28eda087
somoye123 <somoye.ayotunde@gmail.com>
2020-11-05 22:50:30 +0100

    refactor users controller to allow show and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
   def show
+    @user = User.find(params[:id])
     render json: @user
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
 
@@ -24,28 +15,8 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:username, :email)
   end
