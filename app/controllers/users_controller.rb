88a512b179c9506ed9f612361d8fde80432e037d
somoye123 <somoye.ayotunde@gmail.com>
2020-11-05 23:33:53 +0100

    update logic of the users controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,13 @@ class UsersController < SecuredController
     @user = User.find_by(email: params[:email])
     return render json: @user, status: :ok if @user
 
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    user = User.create!(user_params)
+    render json: user, status: :created
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.permit(:username, :email)
   end
 end
