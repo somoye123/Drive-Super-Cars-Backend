906cd6e6e4cc4cb1194e5c369ea8f6fe1ee82f24
somoye123 <somoye.ayotunde@gmail.com>
2020-11-05 23:03:55 +0100

    fix linter error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < SecuredController
-
   def create
     @user = User.find_by(email: params[:email])
     return render json: @user, status: :ok if @user
-    
-    @user = User.new(user_params)
 
+    @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created, location: @user
     else
