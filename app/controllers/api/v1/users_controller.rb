a1fcea2964284f785e40848d7ce72c3865fd319b
somoye123 <somoye.ayotunde@gmail.com>
2020-11-06 11:49:08 +0100

    remove index action from users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,5 @@
 module Api::V1
   class UsersController < SecuredController
-    skip_before_action :authorize_request, only: [:index]
-    def index
-      render json: User.all, status: :created
-    end
     def create
       @user = User.find_by(email: params[:email])
       return render json: @user, status: :ok if @user
