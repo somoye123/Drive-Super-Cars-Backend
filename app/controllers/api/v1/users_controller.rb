7f10b12459225a23fcb41824bebac7eb8711e3f6
somoye123 <somoye.ayotunde@gmail.com>
2020-11-06 09:16:45 +0100

    fix linter error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,17 @@
 module Api::V1
-    class UsersController < SecuredController
-        def create
-          @user = User.find_by(email: params[:email])
-          return render json: @user, status: :ok if @user
-      
-          user = User.create!(user_params)
-          render json: user, status: :created
-        end
-      
-        private
-      
-        def user_params
-          params.permit(:username, :email)
-        end
-      end
+  class UsersController < SecuredController
+    def create
+      @user = User.find_by(email: params[:email])
+      return render json: @user, status: :ok if @user
+
+      user = User.create!(user_params)
+      render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+      params.permit(:username, :email)
+    end
   end
+end
