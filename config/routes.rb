2b78bdbf7c8f7eb14eeb48cf5d5422d11b819f77
somoye123 <somoye.ayotunde@gmail.com>
2020-11-06 11:49:33 +0100

    update routes to allow only users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope module: 'api' do
     namespace :v1 do
-      resources :users, only: [:create,:index]
+      resources :users, only: [:create]
     end
   end
 end
