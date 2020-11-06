b7eac96ab77628b8d1854e4eaafc3b28e637ba71
somoye123 <somoye.ayotunde@gmail.com>
2020-11-06 09:16:18 +0100

    update route to suite versioning api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: 'api' do
+    namespace :v1 do
+      resources :users, only: [:create]
+    end
+  end
 end
