74fa59ec45c22bf57d51e94af8018f0730706f04
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 15:52:48 +0100

    build user controller new_appointment action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,19 @@
 module Api::V1
   class UsersController < SecuredController
-  before_action :set_user, only: %i[create new_appointment]
+    before_action :set_user, only: %i[create new_appointment]
 
     def create
       return render json: @user, status: :ok if @user
 
       user = User.create!(user_params)
       render json: user, status: :created
+    end
+
+    def new_appointment
+      @user = User.find_by(id: appointment_params[:user_id])
+      @car = Car.find_by(id: appointment_params[:car_id])
+      appointment = Appointment.create!(appointment_params)
+      render json: appointment, status: :created
     end
 
     private
@@ -17,6 +24,10 @@ module Api::V1
 
     def user_params
       params.permit(:username, :email)
+    end
+
+    def appointment_params
+      params.permit(:car_id, :user_id)
     end
   end
 end
