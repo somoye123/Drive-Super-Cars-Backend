157dc8633bf417bd2a68c9e0386e3173c4f45430
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 02:04:29 +0100

    add create action to car resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   scope module: 'api' do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :cars, only: %i[index show]
+      resources :cars, only: %i[index show create]
     end
   end
 end
