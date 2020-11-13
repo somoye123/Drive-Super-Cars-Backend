6ea4e2071d55df7d4d9349068ae22ced1b9a14ff
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 15:53:47 +0100

    remove new_appointment action from set_user before_action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 module Api::V1
   class UsersController < SecuredController
-    before_action :set_user, only: %i[create new_appointment]
+    before_action :set_user, only: %i[create]
 
     def create
       return render json: @user, status: :ok if @user
