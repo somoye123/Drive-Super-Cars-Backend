e108c0b3b3f0d3d65512fc8f81dde258a0384073
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 01:34:18 +0100

    update user model with appointment association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :appointments
+  has_many :cars, through: :appointments
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates_uniqueness_of :username
   validates :username, :email, presence: true
