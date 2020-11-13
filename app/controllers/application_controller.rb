72fcabf800def5d4735a1ea8b5bf93c98b02a9a4
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 02:42:47 +0100

    add error handling  for RecordInvalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
 
   private
 
   def not_found(errors)
     render json: errors, status: 404
+  end
+
+  def invalid(errors)
+    render json: errors, status: :unprocessable_entity
   end
 end
