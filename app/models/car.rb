a3d16fef4934bd4481c9777af89c1a02fa27be7f
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 01:18:38 +0100

    build application car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,7 @@
 class Car < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  has_one_attached :image
+  def img_url
+    Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
+  end
 end
--- spec/requests/cars_request_spec.rb
+++ spec/requests/cars_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe "Cars", type: :request do
-
+RSpec.describe 'Cars', type: :request do
 end
