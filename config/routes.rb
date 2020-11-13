a356b0bff96694a6582bf3e4c454ffb919e9998d
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 16:05:06 +0100

    add routing for users new_appointment action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       resources :cars, only: %i[index show create]
+      post 'appointments', to: 'users#new_appointment'
     end
   end
 end
