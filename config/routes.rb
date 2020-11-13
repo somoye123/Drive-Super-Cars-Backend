cce9e654ba4a68fb8633a83965cb15d71b84bd94
somoye123 <somoye.ayotunde@gmail.com>
2020-11-14 00:29:05 +0100

    add routing for fetching user appointment

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
       resources :cars, only: %i[index show create]
       post 'appointments', to: 'users#new_appointment'
+      get 'appointments', to: 'users#all_appointment'
     end
   end
 end
