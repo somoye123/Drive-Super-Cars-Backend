0de6d723122927da2a39e136f8d29cb823712623
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 01:33:33 +0100

    update car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,9 @@
 class Car < ApplicationRecord
-  has_many :appointments, dependent: :destroy
+  has_many :appointments
+  has_many :users, through: :appointments
+
   has_one_attached :image
+
   def img_url
     Rails.application.routes.url_helpers.rails_blob_path(image, only_path: true)
   end
