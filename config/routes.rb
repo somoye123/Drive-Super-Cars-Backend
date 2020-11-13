726063c9189333eb40db92efaa8c3786c9a510b5
somoye123 <somoye.ayotunde@gmail.com>
2020-11-14 00:40:51 +0100

    add routing for users controller show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   resources :cars
   scope module: 'api' do
     namespace :v1 do
-      resources :users, only: [:create]
+      resources :users, only: %i[:create :show]
       resources :cars, only: %i[index show create]
       post 'appointments', to: 'users#new_appointment'
-      get 'appointments', to: 'users#all_appointment'
     end
   end
 end
