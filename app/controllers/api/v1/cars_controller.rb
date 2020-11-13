882efea37d5a20a23ee3d7eade7d940ffbdbf55b
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 01:49:43 +0100

    add create action to cars resources

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,2 +1,19 @@
-class CarsController < ApplicationController
+module Api::V1
+  class CarsController < SecuredController
+    def index
+      @houses = House.all
+      render json: @houses
+    end
+
+    def create
+      house = House.create!(house_params)
+      render json: house, status: :created
+    end
+
+    private
+
+    def house_params
+      params.permit(:photo, :description, :category, :image)
+    end
+  end
 end
