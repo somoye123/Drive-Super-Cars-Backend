130497475bf12f17c0e3fdbad341e12efd1948f6
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 02:24:40 +0100

    add error handling for recordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(errors)
+    render json: errors, status: 404
+  end
 end
