455a7452d59565a6fc9373c5ba95b20843ac57e9
somoye123 <somoye.ayotunde@gmail.com>
2020-11-13 01:44:38 +0100

    add resaources for car

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope module: 'api' do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :cars, only: %i[index show]
     end
   end
 end
