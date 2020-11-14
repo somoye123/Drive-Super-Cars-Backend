9657276a6f3c09e291051c40e35885b9ad27efda
somoye123 <somoye.ayotunde@gmail.com>
2020-11-14 23:57:21 +0100

    add environment variables for production

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -6,16 +6,16 @@ class JsonWebToken
     JWT.decode(token, nil,
                true,
                algorithm: 'RS256',
-               iss: "https://drive-super-cars.us.auth0.com/",
+               iss: 'https://drive-super-cars.us.auth0.com/',
                verify_iss: true,
-               aud: "https://drive-super-cars.herokuapp.com",
+               aud: 'https://drive-super-cars.herokuapp.com',
                verify_aud: true) do |header|
       jwks_hash[header['kid']]
     end
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("https://drive-super-cars.us.auth0.com/.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI('https://drive-super-cars.us.auth0.com/.well-known/jwks.json')
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
