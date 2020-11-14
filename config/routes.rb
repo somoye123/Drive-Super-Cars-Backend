7d45c680ccc127d0c27b055ac35b5d9e992d2805
somoye123 <somoye.ayotunde@gmail.com>
2020-11-14 01:27:02 +0100

    update routing for users resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :cars
   scope module: 'api' do
     namespace :v1 do
-      resources :users, only: %i[:create :show]
+      resources :users, only: %i[create show]
       resources :cars, only: %i[index show create]
       post 'appointments', to: 'users#new_appointment'
     end
