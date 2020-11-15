bed95c17f0a06a0312f42755d0b4baf81a8d46a9
somoye123 <somoye.ayotunde@gmail.com>
2020-11-15 16:01:23 +0100

    add img_url method to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,14 @@
 class Car < ApplicationRecord
+  include Rails.application.routes.url_helpers
   has_many :appointments
   has_many :users, through: :appointments
 
   has_one_attached :image
 
   validates :name, :description, :image, presence: true
+  
+  def img_url
+    url_for(self.image)
+  end
+
 end
