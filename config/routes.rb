f58004f264f453ea99e7813ed538a0373b952b35
somoye123 <somoye.ayotunde@gmail.com>
2020-11-15 14:38:38 +0100

    update routing to active storage blob

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "api/v1/users#index"
-  get '/active_storage/blobs/:signed_id/*filename', to: 'active_storage/blobs#show'
+  get 'rails/active_storage/blobs/:signed_id/*filename', to: 'active_storage/blobs#show'
   scope module: 'api' do
     namespace :v1 do
       resources :users, only: %i[create show]
