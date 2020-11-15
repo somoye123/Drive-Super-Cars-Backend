7d9c8c1e4dc7b5758b0be168e53619df713a6748
somoye123 <somoye.ayotunde@gmail.com>
2020-11-15 18:36:43 +0100

    update default url option for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  Rails.application.routes.default_url_options[:host] = 'localhost:3001'
+  Rails.application.routes.default_url_options = { host: "http://localhost:3001" }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
