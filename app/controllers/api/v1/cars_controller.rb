dc738e76de44b912324972bde1dfcd4c67d61c2b
somoye123 <somoye.ayotunde@gmail.com>
2020-11-15 01:27:18 +0100

    Merge branch 'master' of https://git.heroku.com/drive-super-cars into feature

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -4,9 +4,20 @@ module Api::V1
       render json: Car.all
     end
 
+    def create
+      car = Car.create!(car_params)
+      render json: car, status: :created
+    end
+
     def show
       @car = Car.find(params[:id])
       return render json: @car, status: :ok if @car
+    end
+
+    private
+
+    def car_params
+      params.permit(:description, :name, :image)
     end
   end
 end
