006b2197505dba763fed095826e766b8432ed245
somoye123 <somoye.ayotunde@gmail.com>
2020-11-16 15:22:00 +0100

    add test cases for car model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
   end
 
   describe 'Validations' do
-    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
