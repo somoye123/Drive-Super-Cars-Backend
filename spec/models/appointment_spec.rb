b1bf5ebedbb0bb668c3f012b85cb9afecb0a17dc
somoye123 <somoye.ayotunde@gmail.com>
2020-11-16 15:24:33 +0100

    add test cases for appointment model

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:car) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:car_id) }
+  end
 end
