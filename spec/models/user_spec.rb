1dcf680939c4b50cd63ba867a8bb84b9c8b2a91a
somoye123 <somoye.ayotunde@gmail.com>
2020-11-16 15:18:35 +0100

    add user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should have_many(:appointments) }
+    it { should have_many(:cars).through(:appointments) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+  end
 end
