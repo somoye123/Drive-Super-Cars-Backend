8c661ab48094eb349ef37e524455d6c688a8056f
somoye123 <somoye.ayotunde@gmail.com>
2020-11-18 13:48:07 +0100

    add validation for city and date model fields

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,5 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :car
 
-  validates :user_id, :car_id, presence: true
+  validates :user_id, :car_id, :date, :city, presence: true
 end
